5e6258bb2063b5e87786c2dab78122a38836031c
Kurt W. Allen <KurtA@Gjoll.com>
2022-10-25 09:56:12 -0500

    Cleanup

--- Projects/Clean.bat
+++ Projects/Clean.bat
@@ -1,11 +1,11 @@
-rmdir /s /q build\input
-rmdir /s /q build\output
-rmdir /s /q build\temp
-rmdir /s /q build\template
+rmdir /s /q IG\output
+rmdir /s /q IG\temp
+rmdir /s /q IG\template
+rmdir /s /q IG\fsh-generated
 
-rmdir /s /q FSH\Extensions
-rmdir /s /q FSH\Profiles
-rmdir /s /q FSH\Terminology
+rmdir /s /q IG\FSH\Extensions
+rmdir /s /q IG\FSH\Profiles
+rmdir /s /q IG\FSH\Terminology
 
 rmdir /s /q GraphFiles\Profiles
 rmdir /s /q GraphFiles\Extensions
